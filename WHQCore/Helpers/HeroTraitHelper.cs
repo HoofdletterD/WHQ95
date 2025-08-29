23b5a8fa168370e1e46f01efbb7917ab56044bc9
HoofdletterD <Danieldebruijn@gmail.com>
2025-08-29 15:00:29 +0200

    Add files via upload

---
+++ WHQCore/Helpers/HeroTraitHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using WHQCore.Models.Enums;
+
+namespace WHQCore.Helpers;
+
+public static class HeroTraitHelper
+{
+    public static IEnumerable<HeroTrait> GetTraitsByCategory(TraitCategory category)
+    {
+        return Enum.GetValues<HeroTrait>()
+            .Where(t =>
+            {
+                var attr = t.GetType().GetField(t.ToString())?
+                    .GetCustomAttribute<TraitCategoryAttribute>();
+                return attr?.Category == category;
+            });
+    }
+}
